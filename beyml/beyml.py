e8fc0fb77dc8515be291b0cd4963704d34ab35f3
Elad Maimoni <elad.maimoni@beyeonics.com>
2020-04-28 13:32:12 +0300

    completed module 3 in keras course

--- beyml/beyml.py
+++ beyml/beyml.py
@@ -26,18 +26,39 @@ def main():
     X, y = make_blobs(n_samples=1000, centers=2, random_state=42)
 
     # split the sets into train & validate
-    X_train, X_validate, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_train, X_validate, y_train, y_validate = train_test_split(X, y, test_size=0.3, random_state=42)
 
     # create the model
     model = Sequential()
     # add layers
-    model.add(Dense(units=1, activation='sigmoid', input_shape=(2,)))
+    model.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
+
+    model.compile(Adam(lr=0.05), 'binary_crossentropy', metrics=['accuracy'])
+
+    model.fit(X_train, y_train, epochs=100, verbose=1)
     #data = kbe.variable(np.random.random((4, 2)))
     #zeros_data = kbe.zeros_like(data)
     #res = kbe.eval(zeros_data)
 
-    plt.figure(figsize=(12, 8))
+    aa, bb = plh.get_meshgrid_2d_learning_set(X)
+
+    ab_combinations = np.array([aa.ravel(),bb.ravel()]).T
+
+    model_predictions = model.predict(ab_combinations).reshape(aa.shape)
+    
+    evaluation_result = model.evaluate(X_validate, y_validate)
+
+    print('test loss = ', evaluation_result[0], ' accuracy = ', evaluation_result[1])
+
+
+    # plot countours to show model performance
+    plh.plot_filled_contour_around_predictions(plt, aa, bb, model_predictions)  
+    
+    # plot learning set 
     plh.plot_labeled_features(plt, X, y)
+
+    #plt.figure(figsize=(12, 8))
+
     plt.show()
         
     #print(res)
--- beyml/plot_helpers/labeled_features2d_plot.py
+++ beyml/plot_helpers/labeled_features2d_plot.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def plot_labeled_features(plt, X, y):
     '''
@@ -11,6 +12,7 @@ def plot_labeled_features(plt, X, y):
     
     if (X.shape[0] != y.shape[0] or X.shape[1] != 2):
         raise RuntimeError("incompatible dimensions for plot")
+
     plt.plot(X[y==0, 0], X[y==0, 1], 'ob', alpha=0.5)
     
     # same way, plot all instances labeled 1
@@ -21,18 +23,32 @@ def plot_labeled_features(plt, X, y):
 
     return plt
 
-def plot_decision_boundary(model, X, y):
+
+
+
+def get_meshgrid_2d_learning_set(X, margin = 0.1, tick_per_axis = 101):
+    '''
+    given a 2d feature set X, this function
+    returns a meshgrid that represents the 2d space covered by this set
+    '''
+    # extract the minimal value of each column, 
+    # axis=0 means that we find the minimal value when running through the rows.
+    # there are 2 columns for each row, so we get 2 values that are the minimus on each column
+
+    amin, bmin = X.min(axis=0) - margin
+    amax, bmax = X.max(axis=0) + margin
+
+    horizontal_ticks = np.linspace(amin, amax, tick_per_axis)
+    vertical_ticks = np.linspace(bmin, bmax, tick_per_axis)
     
-    # extract the minimal value of each column, axis 0 means that we treat the array
-    # as a multiaxis table, so that we obtain the min value for each row
-    MARGIN = 0.1
-    TICKS_COUNT = 101
-    amin, amax = X.min(axis=0) - MARGIN
-    amax, bmax = X.max(axis=0) - MARGIN
+    aa, bb = np.meshgrid(horizontal_ticks, vertical_ticks) # aa.shape = bb.shape = (TICKS_COUNT, TICKS_COUNT)
 
-    horizontal_ticks = np.linespace(amin, amax, 101)
-    vertical_ticks = np.linspace(bmin, bmax, 101)
-    aa, bb = np.meshgrid(horizontal_ticks, vertical_ticks)
-    pass
+    # aa runs horizontaly and duplicated verticaly
+    # bb runs verticaly and duplicated horizontaly
+
+    return aa, bb
 
 
+def plot_filled_contour_around_predictions(plt, xx, yy, predictions):
+    plt.contourf(xx,yy,predictions, cmap='bwr', alpha=0.2)
+    return plt
